145793e1174ccfe74adf26ea545ddd341824b986
unknown <pedro.avare01@gmail.com>
2025-08-19 19:32:15 -0300

    adds initial frontend project

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './Screens/Dashboard';
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<Dashboard />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ Frontend/src/Screens/Dashboard/index.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export default function Dashboard() {
+  const navigate = useNavigate();
+
+  const steps = [
+    { name: 'Estrutura do Banco', path: '/upload-json' },
+    { name: 'Importar CSVs', path: '/upload-csv' },
+    { name: 'Mapear Campos', path: '/map-fields' },
+    { name: 'Gerar SQL', path: '/generate-sql' },
+  ];
+
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-4 gap-4 mb-12'>
+      {steps.map((step) => (
+        <button
+          key={step.path}
+          className={`px-6 py-4 rounded shadow font-semibold ${
+            location.pathname === step.path
+              ? 'bg-blue-500 text-white'
+              : 'bg-white text-gray-700'
+          }`}
+          onClick={() => navigate(step.path)}
+        >
+          {step.name}
+        </button>
+      ))}
+    </div>
+  );
+}
